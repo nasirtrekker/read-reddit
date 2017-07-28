9b694d6500d7586e5e142ef8416c16dcd5d7c84f
Nasir Uddin <nasiru32@gmail.com>
2017-07-28 17:29:35 +0200

    news feed thumbnail funcion added

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,21 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+(function() {
+var app = angular.module('read-reddit', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.controller('RedditCtrl', function($http, $scope){
+  $scope.stories = [];
+  $http.get("https://www.reddit.com/r/Android/new/.json").success(function(response){
+      angular.forEach(response.data.children, function(child){
+        $scope.stories.push(child.data);
+      });
+  });
+
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
+    if(window.cordova && window.cordova.plugins.Keyboard) {     
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);     
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
@@ -22,3 +23,4 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+}());
